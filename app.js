46e54b74e01e0485e4b09f0aca3474bb3add5c17
NIKHIL JAIN <nikhiljain.ashajain@gmail.com>
2019-10-08 10:26:32 +0530

    remove node modules & add new files

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const app =express();
 const morgan = require('morgan');//gives log
 const bodyparser =require('body-parser');
 const mongoose = require('mongoose');
+const http = require('http');
+const port = process.env.PORT || 3000;
 
 const productRoutes = require('./api/routes/product');
 const orderRoutes = require('./api/routes/order');
 const userRoutes = require('./api/routes/user');
-mongoose.connect('mongodb://<username>:'+process.env.pw+'@ds151076.mlab.com:51076/rest-api',{useNewUrlParser: true});
+
+//database connection
+mongoose.connect(`mongodb://<username>:${process.env.pw}@ds151076.mlab.com:51076/rest-api`, {useNewUrlParser: true, useFindAndModify: false}, (err) => {
+  if (err) console.error.bind(console, 'connection error: ');
+  console.log("Connected to Data Base");
+});
 
 mongoose.Promise=global.Promise;
 app.use(morgan('dev'));
@@ -29,6 +36,7 @@ next();
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
 app.use('/users',userRoutes);
+
 // req other than above rotes should get error
 app.use((req,res,next) => {
     const error = new Error('Not found');
@@ -36,12 +44,21 @@ app.use((req,res,next) => {
     next(error);
 });
 
-app.use((error,req,res,next)=>{
-    res.status(error.status || 500);
-    res.json({
-        error:{
-            message:error.message
-        }
-    });
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+  // render the error page
+  if (err.status == undefined)
+      res.status(500).render('error-500');
+  else
+    res.status(err.status || 503)
+        .render(`error-${err.status}`);
 });
-module.exports = app;
+
+const server = http.createServer(app);
+server.listen(port, ()=>{
+    console.log("Server Started on port 3000");
+});
